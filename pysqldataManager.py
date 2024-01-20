1b0a3366006ab2c0d38b505f50814c2ad9c3977b
nithin2466 <153836018+nithin2466@users.noreply.github.com>
2024-01-20 15:04:13 +0530

    pysqldataManager.py

--- pysqldataManager.py
+++ pysqldataManager.py
@@ -4,8 +4,10 @@ import sqlite3
 import tkinter.messagebox as msgbox
 
 n=Tk()
+#example - student registration
 n.title("Student Registration")
 n.geometry('500x500')
+
 #insert function
 def Insert():
     id=stid_entry.get()
@@ -14,14 +16,14 @@ def Insert():
     if(id=="" or name=="" or phno==""):
         msgbox.showinfo("Alert","fill all fields")
     else:
-        con=mysql.connect(host="localhost",user="root",password="Hey123",database="student")
+        con=mysql.connect(host="localhost",user="root",password="YOUR_PASSWORD",database="student")
         con=sqlite3.connect("studentdb.db")
         cur=con.cursor()
-        
-        #cur.execute("create table person(id int,name varchar(20),phno int(11))")
-        cur.execute("insert into person values('"+id+"','"+name+"','"+phno+"')")
+        cur.execute("create table student(id int,name varchar(20),phno int(11))")
+        cur.execute("insert into student values('"+id+"','"+name+"','"+phno+"')")
         cur.execute("Commit")
         msgbox.showinfo("info","record inserted successfully")
+
 #delete function
 def Delete():
     id=stid_entry.get()
@@ -41,7 +43,6 @@ def Reset():
     stphno_entry.delete(0, 'end')
 
 #view function
-#view function
 def View():
     con=sqlite3.connect("studentdb.db")
     cur=con.cursor()
@@ -51,6 +52,7 @@ def View():
     for row in rows:
         result += 'ID: ' + str(row[0]) + ', Name: ' + str(row[1]) + ', Phone: ' + str(row[2]) + '\n'
     msgbox.showinfo("Records", result)
+
 #update function
 def Update():
     id=stid_entry.get()
@@ -69,7 +71,6 @@ def Update():
 updatebtn=Button(n,text="UPDATE",font=("Verdana",16),command=Update)
 updatebtn.grid(row=7,column=1,padx=10,pady=10)
 
-
 #delete button
 deletebtn=Button(n,text="DELETE",font=("Verdana",16),command=Delete)
 deletebtn.grid(row=4,column=1,padx=10,pady=10)
@@ -87,18 +88,22 @@ stid=Label(n,text="Student Id:",font=("Verdana",16))
 stid.grid(row=0,column=0)
 stid_entry=Entry(n,font=("Verdana",16))
 stid_entry.grid(row=0,column=1,padx=10,pady=10)
+
 #student name field
 stname=Label(n,text="Student Name:",font=("Verdana",16))
 stname.grid(row=1,column=0)
 stname_entry=Entry(n,font=("Verdana",16))
 stname_entry.grid(row=1,column=1,padx=10,pady=10)
+
 #student phno field
 stphno=Label(n,text="Student phno:",font=("Verdana",16))
 stphno.grid(row=2,column=0)
 stphno_entry=Entry(n,font=("Verdana",16))
 stphno_entry.grid(row=2,column=1,padx=10,pady=10)
+
 #button
 insertbtn=Button(n,text="INSERT",font=("Verdana",16),command=Insert)
 insertbtn.grid(row=3,column=1,padx=10,pady=10)
 
 n.mainloop()
+
